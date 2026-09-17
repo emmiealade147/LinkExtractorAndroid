69b343439e1747bd309219f941acdb0e706c82e6
emmiealade147 <emmiealade147@gmail.com>
2026-09-17 07:29:48 +0100

    Update MainActivity.kt

--- app/src/main/kotlin/com/linkextractor/app/MainActivity.kt
+++ app/src/main/kotlin/com/linkextractor/app/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
